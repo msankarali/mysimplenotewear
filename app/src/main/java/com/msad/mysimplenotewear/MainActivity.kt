10d49111c82ffc1e633b8f129d70d3b88a8e5ecb
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-02-18 01:33:36 +0300

    icon and codes added

--- app/src/main/java/com/msad/mysimplenotewear/MainActivity.kt
+++ app/src/main/java/com/msad/mysimplenotewear/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.msad.mysimplenotewear
 
+import android.content.Context
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : WearableActivity() {
 
@@ -9,7 +11,14 @@ class MainActivity : WearableActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setAmbientEnabled()
+        val sharedPreferences = this.getSharedPreferences(packageName, Context.MODE_PRIVATE)
+        val a: String? = sharedPreferences.getString("note", "")
+        editText.setText(a)
+    }
 
-
+    override fun onPause() {
+        super.onPause()
+        val sharedPreferences = this.getSharedPreferences(packageName, Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString("note", editText.text.toString()).apply()
     }
 }
