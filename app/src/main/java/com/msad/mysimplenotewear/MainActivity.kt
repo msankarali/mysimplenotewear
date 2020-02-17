0b4f1ef71b1aedf8e82df5a42079b223b8d2e782
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-02-18 01:17:18 +0300

    background added

--- app/src/main/java/com/msad/mysimplenotewear/MainActivity.kt
+++ app/src/main/java/com/msad/mysimplenotewear/MainActivity.kt
@@ -8,8 +8,8 @@ class MainActivity : WearableActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Enables Always-on
         setAmbientEnabled()
+
+
     }
 }
